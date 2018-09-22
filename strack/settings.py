987308d8b47ddb52ca5087ee6857e817595bf709
unknown <rudrachaudhary66@gmail.com>
2018-09-22 19:10:29 +0545

    first commit

--- strack/settings.py
+++ strack/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'thanks'
