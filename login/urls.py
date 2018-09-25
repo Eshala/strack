fdc399bdd1b497ae38cb952e9db238d32941a805
unknown <rudrachaudhary66@gmail.com>
2018-09-25 07:31:26 +0545

    User registration is added

--- login/urls.py
+++ login/urls.py
@@ -7,5 +7,5 @@ app_name = 'login'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-
+    path('signup/',views.SignUP.as_view(),name='signup'),
     ]
